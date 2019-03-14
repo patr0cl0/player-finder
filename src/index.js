fe8ce3aa294ea55715dabe164536ac767edfaa64
Fernando Salazar <fernandsalazar3@gmail.com>
2019-03-14 14:28:12 -0300

    setting up redux on index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
 import App from './App';
+import './index.css';
+import store from './redux';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const WholeApp = (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(WholeApp, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
